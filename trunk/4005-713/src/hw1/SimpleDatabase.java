6466cd29fb31b682e1d6ee0fcc33ee438698da39
litcigar <litcigar@16eac640-c486-11dd-ae62-6dab0a2b1684>
2008-12-10 17:20:30 +0000

    Final version....

--- trunk/4005-713/src/hw1/SimpleDatabase.java
+++ trunk/4005-713/src/hw1/SimpleDatabase.java
@@ -1,5 +1,6 @@
 package hw1;
 
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -14,10 +15,12 @@ import java.util.Scanner;
  */
 public class SimpleDatabase {
 
-	protected File fileBase;
+	protected File fileBase = null;
 	protected List<ArrayList<Object>> tupleBase;
 	
 	public static enum Command { ADD, SHOW, DELETE, INVALID, QUIT };
+	
+	
 	
 	/**
 	 * 	Default constructor.
@@ -25,6 +28,8 @@ public class SimpleDatabase {
 	public SimpleDatabase() {
 		tupleBase = new ArrayList<ArrayList<Object>>();		
 	}
+	
+	
 	
 	/**
 	 * Constructor that loads from and maintains the database in the 
@@ -37,6 +42,145 @@ public class SimpleDatabase {
 		this.fileBase = pFileBase;
 		loadTuplesFromFile();
 	}
+	
+	
+	
+	/**
+	 * Add the strings of a string array into the tupleBase as a new tuple.
+	 * @param pStringTuple Strings from which to create a tuple that is to be
+	 * 			inserted into the tupleBase
+	 */
+	public void addTuple( String[] pStringTuple ) {
+		ArrayList<Object> listTuple = new ArrayList<Object>();
+		
+		for( String t : pStringTuple ) {
+			if( t.length() > 0 ) { 
+				listTuple.add( t.replaceAll( "^\\s+|\\s+$", "") ); 
+			}
+		}
+		
+		tupleBase.add( listTuple );
+	}
+	
+	/**
+	 * Adds the strings of a string array into the tupleBase as a new tuple,
+	 * also saves this addition to the filesystem.
+	 * @param pStringTuple
+	 */
+	public void addTupleAndSave( String[] pStringTuple ) {
+		addTuple( pStringTuple );
+		saveTuplesToFile();
+	}
+	
+	
+	
+	/**
+	 * Deletes the specified tuple.
+	 * @param tuple Tuple to be deleted.
+	 */
+	public void deleteTuple( ArrayList<Object> tuple ) {
+		this.tupleBase.remove( tuple );
+	}
+
+	/**
+	 * Deletes all tuples that have an element that matches the sepcified
+	 * regular expression.
+	 * @param regex Regular expression to match against tuples in the
+	 * 			tuplebase.
+	 */
+	public void deleteTuple( String regex ) {
+		this.tupleBase.removeAll( getTuples( regex ) );
+	}
+	
+	/**
+	 * Deletes all tuples from the specific list.
+	 * @param tuples Tuples to be deleted.
+	 */
+	public void deleteTuples( List<ArrayList<Object>> tuples ) {
+		this.tupleBase.removeAll( tuples );
+	}
+	
+	
+	
+	/**
+	 * Displays the tuple specified.
+	 * @param tuple Tuple to be displayed.
+	 */
+	public void displayTuple( ArrayList<Object> tuple ) {
+		String[] tupleString = new String[ tuple.size() ];
+		displayTuple( tuple.toArray( tupleString ) );
+	}
+	
+	/**
+	 * Displays the specified tuple.
+	 * @param pTuple Tuple to be displayed.
+	 */
+	public void displayTuple( String[] pTuple ) {
+		for( int i = 0; i < pTuple.length; ++i ) {
+			if( pTuple[ i ].length() > 0 ) {
+				System.out.print( pTuple[ i ] );
+				if( i + 1 < pTuple.length ) {
+					System.out.print( " " );
+				}
+			}
+		}
+		System.out.println();
+	}
+	
+	/**
+	 * Displays all tuples stored in the database.
+	 */
+	public void displayTuples() {
+		for( ArrayList<Object> tuple : tupleBase ) {
+			displayTuple( tuple );
+		}
+	}
+	
+	/**
+	 * Displays the list of specified tuples.
+	 * @param tuples List of tuples to be displayed.
+	 */
+	public void displayTuples( List<ArrayList<Object>> tuples ) {
+		for( ArrayList<Object> tuple : tuples ) {
+			displayTuple( tuple );
+		}
+	}
+	
+	/**
+	 * Display those tuples which have an element that matches the provided
+	 * regular expression.
+	 * @param regex Regular expression to match against tuple elements.
+	 */
+	public void displayTuples( String regex ) {
+		displayTuples( getTuples( regex ) );
+	}
+	
+	
+	
+	/**
+	 * Obtain those tuples which have an element that matches the provided
+	 * regular expression.
+	 * @param regex Regular expression to match against tuple elements.
+	 */
+	public List<ArrayList<Object>> getTuples( String regex ) {
+		List<ArrayList<Object>> tupleMatches = new ArrayList<ArrayList<Object>>();
+		
+		for( ArrayList<Object> tuple : tupleBase ) {
+			for( Object element : tuple ) {
+				// check to see whether this element matches, if it does
+				// display the entire tuple and move on to the next tuple
+				// in the database
+				if( element.toString().matches( regex ) ) {
+					tupleMatches.add( tuple );
+					break;
+				}
+			}
+		}
+		
+		return tupleMatches;
+	}
+	
+	
 	
 	/**
 	 * Loads tuples from the tuplebase file associated with this simpledatabase
@@ -75,20 +219,22 @@ public class SimpleDatabase {
 
 	/**
 	 * Save the active memory tuplebase to the filesystem.
+	 * @return True if the save operation succeeding, false otherwise.
 	 */
-	public void saveTuplesToFile() {
-		saveTuplesToFile( this.fileBase );
+	public boolean saveTuplesToFile() {
+		return saveTuplesToFile( this.fileBase );
 	}
 	
 	/**
 	 * Save the active memory tuplebase to the specified file in the 
 	 * filesystem.
 	 * @param pFile File into which the tuplebase will be stored.
+	 * @return True if the save operation succeeding, false otherwise.
 	 */
-	public void saveTuplesToFile( File pFile ) {
+	public boolean saveTuplesToFile( File pFile ) {
 		// fail fast
 		if( pFile ==  null ) {
-			return;
+			return false;
 		}
 		
 		
@@ -105,233 +251,182 @@ public class SimpleDatabase {
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			System.err.println( "Unable to save to file: " + e.getMessage() );
+			return false;
 		}
-	}
-	
-	
-	
-	/**
-	 * Add the strings of a string array into the tupleBase as a new tuple.
-	 * @param pStringTuple Strings from which to create a tuple that is to be
-	 * 			inserted into the tupleBase
-	 */
-	public void addTuple( String[] pStringTuple ) {
-		ArrayList<Object> listTuple = new ArrayList<Object>();
-		
-		for( String t : pStringTuple ) {
-			if( t.length() > 0 ) { 
-				listTuple.add( t.replaceAll( "^\\s+|\\s+$", "") ); 
-			}
-		}
-		
-		tupleBase.add( listTuple );
-	}
-	
-	/**
-	 * Adds the strings of a string array into the tupleBase as a new tuple,
-	 * also saves this addition to the filesystem.
-	 * @param pStringTuple
-	 */
-	public void addTupleAndSave( String[] pStringTuple ) {
-		addTuple( pStringTuple );
-		saveTuplesToFile();
-	}
-	
-	/**
-	 * Displays all tuples stored in the database.
-	 */
-	public void displayTuples() {
-		for( ArrayList<Object> tuple : tupleBase ) {
-			displayTuple( tuple );
-		}
-	}
-	
-	/**
-	 * Displays the tuple specified.
-	 * @param tuple Tuple to be displayed.
-	 */
-	public void displayTuple( ArrayList<Object> tuple ) {
-		String[] tupleString = new String[ tuple.size() ];
-		displayTuple( tuple.toArray( tupleString ) );
-	}
-	
-	/**
-	 * Displays the specified tuple.
-	 * @param pTuple Tuple to be displayed.
-	 */
-	public void displayTuple( String[] pTuple ) {
-		for( int i = 0; i < pTuple.length; ++i ) {
-			if( pTuple[ i ].length() > 0 ) {
-				System.out.print( pTuple[ i ] );
-				if( i + 1 < pTuple.length ) {
-					System.out.print( " " );
-				}
-			}
-		}
-		//System.out.println();
-	}
-	
-	/**
-	 * Displays the list of specified tuples.
-	 * @param tuples List of tuples to be displayed.
-	 */
-	public void displayTuples( List<ArrayList<Object>> tuples ) {
-		for( ArrayList<Object> tuple : tuples ) {
-			displayTuple( tuple );
-		}
-	}
-	
-	/**
-	 * Display those tuples which have an element that matches the provided
-	 * regular expression.
-	 * @param regex Regular expression to match against tuple elements.
-	 */
-	public void displayTuples( String regex ) {
-		displayTuples( getTuples( regex ) );
-	}
-	
-	/**
-	 * Obtain those tuples which have an element that matches the provided
-	 * regular expression.
-	 * @param regex Regular expression to match against tuple elements.
-	 */
-	public List<ArrayList<Object>> getTuples( String regex ) {
-		List<ArrayList<Object>> tupleMatches = new ArrayList<ArrayList<Object>>();
-		
-		for( ArrayList<Object> tuple : tupleBase ) {
-			for( Object element : tuple ) {
-				// check to see whether this element matches, if it does
-				// display the entire tuple and move on to the next tuple
-				// in the database
-				if( element.toString().matches( regex ) ) {
-					tupleMatches.add( tuple );
-					break;
-				}
-			}
-		}
-		
-		return tupleMatches;
-	}
-	
-	/**
-	 * Deletes the specified tuple.
-	 * @param tuple Tuple to be deleted.
-	 */
-	public void deleteTuple( ArrayList<Object> tuple ) {
-		this.tupleBase.remove( tuple );
+		return true;
 	}
 
-	/**
-	 * Deletes all tuples from the specific list.
-	 * @param tuples Tuples to be deleted.
-	 */
-	public void deleteTuples( List<ArrayList<Object>> tuples ) {
-		this.tupleBase.removeAll( tuples );
-	}
+	
 	
 	/**
-	 * Deletes all tuples that have an element that matches the sepcified
-	 * regular expression.
-	 * @param regex Regular expression to match against tuples in the
-	 * 			tuplebase.
+	 * Main program.
+	 * @param args Filename from which to load the database.  Optional.
 	 */
-	public void deleteTuple( String regex ) {
-		this.tupleBase.removeAll( getTuples( regex ) );
-	}
-	
 	public static void main( String... args )  {
+		// display greeting
 		System.out.println("== Welcome to Simple Database! ==");
-		System.out.println("command: add, show, delete, quit");
+		System.out.println(
+				"Commands:\nadd {tuple}, \nshow {description}, " +
+				"\ndelete {description}, \nquit {filename*}");
 
+		
+		// create database
 		SimpleDatabase db = new SimpleDatabase();
 		
+		
+		// load database filename if available
 		if( args.length == 1 ) {
 			String fileName = args[ 0 ];
-			System.out.printf( "loading db info from: '%s'\n", fileName );
+			System.out.printf( "Loading db info from: '%s'\n", fileName );
 			db.loadTuplesFromFile( new File( fileName ) );
 		}
 
 				
+		// begin reading from standard in
 		Scanner scanner = new Scanner( System.in );
 		
+		
+		// setup variables for operation
 		Command c = SimpleDatabase.Command.INVALID;
 		String description = "";
+		
+		
+		// accept commands until QUIT has been received, then exit the program
 		while( c != SimpleDatabase.Command.QUIT ) {
+			
+			// pretty up where the user enters text
 			System.out.print("> ");
 			
+			
+			// attempt to understand the command they gave us
 			if( scanner.hasNext() ) { 
 				try {
-				c = Command.valueOf( 
-						SimpleDatabase.Command.class, 
-						scanner.next().toUpperCase() );
+					c = Command.valueOf( 
+							SimpleDatabase.Command.class, 
+							scanner.next().toUpperCase() );
 				}
 				catch( Exception e ) {
 					c = SimpleDatabase.Command.INVALID;
 				}
 			}
 			
+			
+			// perform an operation that is command-dependent
 			switch( c ) {
 			
+				// add the tuple to the db's active memory
 				case ADD :
-					String tupleList = scanner.nextLine();
-					if( tupleList != null ) {
+					String tupleList = 
+						scanner.nextLine().replaceFirst( "^\\s", "" );
+					
+					// if properly formatted
+					if( tupleList != null && !tupleList.equals( "" ) ) {
 						String[] tuple = tupleList.split( " " );
-						//for( String s : tuple ) {
-						//	System.out.println( s );
-						//}
+						
 						System.out.println( "==============" );
-						System.out.print( "Adding tuple: '" );
+						System.out.print( "Adding tuple: " );
+						
 						db.displayTuple( tuple );
-						System.out.println( "'");
+						
 						System.out.println( "==============" );
+						
 						db.addTuple( tuple );
 					}
+					// complain otherwise
 					else {
-						System.err.println( "The 'add' command requires a tuple." );
+						System.out.println( "==============" );
+						System.out.println( 
+								"Use: add {element_1, element_2, " +
+								"... elemnt_n}" );
+						System.out.println( "==============" );
 					}
 					break;
+					
+				// delete tuples which contain an element which matches the 
+				// description pattern that was passed in
 				case DELETE :
-					description = scanner.next();
-					if( description != null ) {
+					description = scanner.nextLine().replaceFirst("^\\s+", "");
+					
+					// if properly formatted
+					if( description != null && !description.equals( "" ) ) {
 						System.out.println( "==============" );
-						System.out.println( "Deleting tuples..." );
+						System.out.printf( 
+								"Deleting tuples that have an element that matches '%s':\n", 
+								description );
 						db.displayTuples( db.getTuples( description ) );
 						System.out.println( "==============" );
 						db.deleteTuple( description );
 					}
+					// complain otherwise
 					else {
-						System.err.println( "The 'delete' command requires a description for tuple selection." );
+						System.out.println( "==============" );
+						System.out.println( "Use: delete {description}" );
+						System.out.println( "==============" );
 					}
 					break;
+					
+				// quit program operation, save the database if we have a file
+				// name to save to
 				case QUIT :
 					System.out.println( "==============" );
-					System.out.println( "Quiting program..." );
-
-					if( scanner.hasNext() ) {
-						String filename = scanner.next();
-						System.out.println( "Saving db to " + filename );
-						db.saveTuplesToFile( new File( filename ) );
+										
+					String filename = 
+						scanner.nextLine().replaceFirst( "^\\s*", "" );
+					
+					// save to the file that was specified
+					if( filename != null && !filename.equals("") ) {
+						if( db.saveTuplesToFile( new File( filename ) ) ) {
+							System.out.printf( 
+									"Saved db to '%s'\n", 
+									filename );
+						}
 					}
+					// save to the file specified at program start otherwise
 					else {
-						db.saveTuplesToFile();
+						if( db.saveTuplesToFile() ) {
+							System.out.printf( 
+									"Saved db to '%s'\n", 
+									db.fileBase );
+						}
 					}
+					
+					System.out.println( "Quiting program..." );
 					System.out.println( "==============" );
 					System.exit( 0 );
 					break;
+					
+				// show those tuples in active memory that have an element that
+				// matches the regular expression pattern
 				case SHOW :
-					description = scanner.next();
-					if( description != null ) {
+					description = scanner.nextLine().replaceFirst( "^\\s*", "" );
+					
+					// if description is properly formatted
+					if( description != null && !description.equals("") ) {
 						System.out.println( "==============" );
-						System.out.println( "Displaying tuples..." );
+						System.out.printf( 
+								"Displaying tuples that have an element that matches '%s':\n", 
+								description );
 						db.displayTuples( description );
 						System.out.println( "==============" );
 					}
+					// complain otherwise
 					else {
-						System.err.println( "The 'show' command requires a description for tuple selection." );
+						System.out.println( "==============" );
+						System.out.println( "Use: show {description}" );
+						System.out.println( "==============" );
 					}
 					break;
+					
+				// catch all of the bad commands and yell at the user
 				case INVALID :	
-					System.err.println( "Please enter a command: 'ADD tuple, SHOW description, DELETE description, or QUIT' ");
+					System.out.println( "==============" );
+					System.out.println( 
+							"Please enter a command: 'ADD {tuple}, " +
+							"SHOW {description}, DELETE {description}, " +
+							"or QUIT {filename*}' ");
+					System.out.println( "==============" );
+					
+					break;
 			}
 		}
 	}
